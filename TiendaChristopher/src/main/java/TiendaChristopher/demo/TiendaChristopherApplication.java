d6e16cb1a14bf3e0bd14bd0ad2fff6f956b6c055
chernandez2607 <chernandez10616@ufide.ac.cr>
2025-09-17 20:38:49 -0600

    Commit 1 y 2

---
+++ TiendaChristopher/src/main/java/TiendaChristopher/demo/TiendaChristopherApplication.java
@@ -0,0 +1,13 @@
+package TiendaChristopher.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaChristopherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaChristopherApplication.class, args);
+	}
+
+}
---
+++ TiendaChristopher/src/test/java/TiendaChristopher/demo/TiendaChristopherApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaChristopher.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaChristopherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
